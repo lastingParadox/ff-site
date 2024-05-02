8f31d4b6a5a928c15c0f97215ec5ca7f5088de18
Zander Preston <zanpres@gmail.com>
2024-05-02 15:29:08 -0400

    Add ColorMode Context, fix Tuck and Run json

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import Navbar from "@/components/Navbar/Navbar";
+import React, { useMemo } from "react";
 
 const darkTheme = createTheme({
     palette: {
@@ -9,12 +10,41 @@ const darkTheme = createTheme({
     },
 });
 
+export const ColorModeContext = React.createContext({
+    colorMode: "dark",
+    toggleColorMode: () => {},
+});
+
+export function ColorModeProvider({ children }: { children: React.ReactNode }) {
+    const [colorMode, setColorMode] = React.useState<"light" | "dark">("dark");
+
+    const toggleColorMode = () => {
+        setColorMode((prevColorMode) =>
+            prevColorMode === "light" ? "dark" : "light"
+        );
+    };
+
+    const theme = useMemo(() => {
+        return createTheme({
+            palette: {
+                mode: colorMode,
+            },
+        });
+    }, [colorMode]);
+
+    return (
+        <ColorModeContext.Provider value={{ colorMode, toggleColorMode }}>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+}
+
 export default function RootLayout() {
     return (
-        <ThemeProvider theme={darkTheme}>
+        <ColorModeProvider>
             <CssBaseline />
             <Navbar />
             <Outlet />
-        </ThemeProvider>
+        </ColorModeProvider>
     );
 }
