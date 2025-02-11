7a7199212eb9ef5ea8584048d4af419acda20488
retribyte <treyroemer@gmail.com>
2025-02-11 13:10:06 -0600

    Add primary & seasonal palette options

--- src/components/EpisodeItem/EpisodeItem.tsx
+++ src/components/EpisodeItem/EpisodeItem.tsx
@@ -1,22 +1,35 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Episode as EpisodeType } from '@/types/Episodes';
 import styles from './episode.module.scss';
+import { palette } from '@mui/system';
 
 // export default function StoryBlock({ block, id }: { block: Block; id: number }): JSX.Element {
-export default function EpisodeItem(
-    { episode, handleEpisodeClick }:
-    { episode: EpisodeType, handleEpisodeClick: (episode: EpisodeType) => void }
-): JSX.Element {
+export default function EpisodeItem({
+    episode,
+    handleEpisodeClick,
+}: {
+    episode: EpisodeType;
+    handleEpisodeClick: (episode: EpisodeType) => void;
+}): JSX.Element {
     return (
         <li key={episode.episode_number} className={styles.episode}>
-            <div className={styles['episode-item']} onClick={() => handleEpisodeClick(episode)}>
-                <Typography variant='h3' className={styles.title}>
+            <Box
+                className={styles['episode-item']}
+                sx={{
+                    backgroundColor: 'action.hover',
+                    '&:hover': {
+                        backgroundColor: 'action.selected',
+                    },
+                }}
+                onClick={() => handleEpisodeClick(episode)}
+            >
+                <Typography variant='h3' className={styles.title} sx={{ color: 'text.primary' }}>
                     {episode.title}
                 </Typography>
-                <Typography variant='subtitle1' className={styles.description}>
+                <Typography variant='subtitle1' className={styles.description} sx={{ color: 'text.secondary' }}>
                     {episode.short_desc}
                 </Typography>
-            </div>
+            </Box>
         </li>
     );
 }
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -10,6 +10,21 @@ export const ColorModeContext = React.createContext<{ colorMode: ColorMode; togg
     toggleColorMode: () => {},
 });
 
+declare module '@mui/material/styles' {
+    interface Palette {
+        ff1: Palette['primary'];
+        ff2: Palette['primary'];
+        ff3: Palette['primary'];
+        ff4: Palette['primary'];
+    }
+    interface PaletteOptions {
+        ff1: PaletteOptions['primary'];
+        ff2: PaletteOptions['primary'];
+        ff3: PaletteOptions['primary'];
+        ff4: PaletteOptions['primary'];
+    }
+}
+
 export function ColorModeProvider({ children }: { children: React.ReactNode }) {
     const [colorMode, setColorMode] = React.useState<ColorMode>(
         (localStorage.getItem('colorMode') as ColorMode) || 'dark'
@@ -27,6 +42,38 @@ export function ColorModeProvider({ children }: { children: React.ReactNode }) {
         return createTheme({
             palette: {
                 mode: colorMode,
+                primary: {
+                    light: '#9582ee',
+                    main: '#5d4be5',
+                    dark: '#002cc2',
+                    contrastText: '#fff',
+                },
+                secondary: {
+                    light: '#f9cd8e',
+                    main: '#F39E3B',
+                    dark: '#d3612c',
+                    contrastText: '#000',
+                },
+                ff1: {
+                    light: '#f9938f',
+                    main: '#f01515',
+                    dark: '#a20000',
+                },
+                ff2: {
+                    light: '#89c6ff',
+                    main: '#0a6bea',
+                    dark: '#1e37b8',
+                },
+                ff3: {
+                    light: '#a5d794',
+                    main: '#47b015',
+                    dark: '#006000',
+                },
+                ff4: {
+                    light: '#d297f9',
+                    main: '#a020f0',
+                    dark: '#571fdc',
+                }
             },
             components: {
                 MuiTypography: {
